7fd3cc332ea72e8e922e8119a6087e987911b87a
benjamin figueroa <benjifig@gmail.com>
2025-12-03 21:37:52 -0600

    zelda is stinky

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "test" << std::endl;
+    std::cout << "Zelda has booties on!" << std::endl;
     return 0;
 }
